89b77dfd9b35d73ffa6ede1c3db53f2356fa0439
none <none>
2016-12-12 19:58:47 +0200

    number 4

---
+++ student.py
@@ -0,0 +1,31 @@
+class Student :
+    def __init__(self,name,hometown,age,height,faviroute_ice_cream_flavor):
+        
+        self.name=name
+        self.hometown=hometown
+        self.age=age
+        self.height=height
+        self.faviroute_ice_cream_flavor=faviroute_ice-cream_flavor
+        
+        def print_summary (self):
+            print (self+"my name is jinan, my hometown is beit safafa , im 15 years old , im 160cm, my faviroute ice cream flavor is vaniella ")
+            
+    
+            
+                
+            
+            
+
+
+            
+            
+            
+        
+
+
+
+
+
+     
+    
+
